a49f0e7366706c0a7f868f13cbbce87b22800452
Apdo3939 <apdo3939@hotmail.com>
2021-09-02 12:30:15 -0300

    Integrate with backend

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Script from 'next/script';
 
-function HomePage() {
+function HomePage({ data }) {
     return (
         <>
             <Head>
@@ -26,48 +26,52 @@ function HomePage() {
                     </div>
                 </div>
             </nav>
-            <section className="top" id="top">
+            <section className="img-top top" id="top">
+                <style>
+                    {`.img-top{
+                        background-image: url(`+ data.url + data.data.image_top + `);
+                        background-repeat: no-repeat;
+                        background-position: center;
+                    }`}
+                </style>
                 <div className="max-width">
                     <div className="top-content">
-                        <div className="text-1">Temos a solução</div>
-                        <div className="text-2">de acordo com a sua empresa</div>
-                        <div className="text-3">Podemos ajudar a sua empresa!</div>
-                        <a href="#contact">Entrar em Contato</a>
+                        <div className="text-1">{data.data.title_top_one}</div>
+                        <div className="text-2">{data.data.title_top_two}</div>
+                        <div className="text-3">{data.data.title_top_three}</div>
+                        <a href="#contact">{data.data.btn_title_top}</a>
                     </div>
                 </div>
             </section>
             <section className="services" id="services">
                 <div className="max-width">
-                    <h2 className="title">Serviços</h2>
+                    <h2 className="title">{data.data.ser_title}</h2>
                     <div className="serv-content">
                         <div className="card">
                             <div className="box">
-                                <i className="fas fa-code"></i>
+                                <i className={data.data.ser_icon_one}></i>
                                 <div className="text">
-                                    All Code
+                                    {data.data.ser_title_one}
                                 </div>
-                                <p>Este serviço é destinado para empresas que necessita de auxilio em sua inclusão num mundo
-                                    digital mais moderno em todas as areas!</p>
+                                <p>{data.data.ser_desc_one}</p>
                             </div>
                         </div>
                         <div className="card">
                             <div className="box">
-                                <i className="fas fa-laptop-code"></i>
+                                <i className={data.data.ser_icon_two}></i>
                                 <div className="text">
-                                    Desktop
+                                    {data.data.ser_title_two}
                                 </div>
-                                <p>Este serviço é destinado para empresas que necessita de auxilio em sua inclusão num mundo
-                                    digital mais moderno na area somente relacionado a web desktop!</p>
+                                <p>{data.data.ser_desc_two}</p>
                             </div>
                         </div>
                         <div className="card">
                             <div className="box">
-                                <i className="fas fa-mobile-alt"></i>
+                                <i className={data.data.ser_icon_three}></i>
                                 <div className="text">
-                                    Mobile
+                                    {data.data.ser_title_three}
                                 </div>
-                                <p>Este serviço é destinado para empresas que necessita de auxilio em sua inclusão num mundo
-                                    digital mais moderno na area somente relacionado a mobile!</p>
+                                <p>{data.data.ser_desc_three}</p>
                             </div>
                         </div>
                     </div>
@@ -75,39 +79,39 @@ function HomePage() {
             </section>
             <section className="contact" id="contact">
                 <div className="max-width">
-                    <h2 className="title">Contato</h2>
+                    <h2 className="title">{data.data.cont_title}</h2>
                     <div className="contact-content">
                         <div className="column left">
                             <p>
-                                Por favor preencher todo o formulário de acordo com suas necessidades da sua empresa!!!
+                                {data.data.cont_desc}
                             </p>
                             <div className="icons">
                                 <div className="row">
-                                    <i className="fas fa-user"></i>
+                                    <i className={data.data.cont_icon_emp}></i>
                                     <div className="info">
-                                        <div className="head">Empresa</div>
-                                        <div className="sub-title">Alex's Info</div>
+                                        <div className="head">{data.data.cont_title_emp}</div>
+                                        <div className="sub-title">{data.data.cont_name_emp}</div>
                                     </div>
                                 </div>
                                 <div className="row">
-                                    <i className="fas fa-map-marker-alt"></i>
+                                    <i className={data.data.cont_icon_end}></i>
                                     <div className="info">
-                                        <div className="head">Endereço</div>
-                                        <div className="sub-title">Rua da felicidade casa 100</div>
+                                        <div className="head">{data.data.cont_title_end}</div>
+                                        <div className="sub-title">{data.data.cont_name_end}</div>
                                     </div>
                                 </div>
                                 <div className="row">
-                                    <i className="fas fa-envelope"></i>
+                                    <i className={data.data.cont_icon_email}></i>
                                     <div className="info">
-                                        <div className="head">Email</div>
-                                        <div className="sub-title">alexinfo@email-exemplo.com</div>
+                                        <div className="head">{data.data.cont_title_email}</div>
+                                        <div className="sub-title">{data.data.cont_name_email}</div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="column right">
                             <div className="text">
-                                Mensagem de contato
+                                {data.data.cont_title_form}
                             </div>
                             <form>
                                 <div className="fields">
@@ -135,13 +139,19 @@ function HomePage() {
             </section>
 
             <footer>
-                <span>Created by <a href="#">Alexandre Pereira</a> </span>
+                <span>{data.data.footer_desc} <a href={data.data.footer_link}>{data.data.footer_name}</a> </span>
             </footer>
 
             <Script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js' strategy="beforeInteractive" />
             <Script src="custom.js" strategy='afterInteractive' />
         </>
     )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch(`http://localhost:8081`);
+    const data = await response.json();
+    return { props: { data } }
 }
 
 export default HomePage
